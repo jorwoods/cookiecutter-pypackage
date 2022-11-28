b7263f8c0cd10dda20be74bc5a356252649f5dc9
Jordan Woods <lenluin@gmail.com>
2022-11-27 21:40:42 -0600

    Use pathlib for path manipulation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import os
+from pathlib import Path
 
-PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+PROJECT_DIRECTORY = Path.cwd().resolve().absolute()
 
 
 def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+    (PROJECT_DIRECTORY / filepath).unlink()
 
 
 if __name__ == '__main__':
@@ -20,3 +21,6 @@ if __name__ == '__main__':
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    if "{{ cookiecutter.package_manager }}" != "pip":
+        remove_file("requirements_dev.txt")
